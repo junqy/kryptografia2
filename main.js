a1917334ccee28437587b277eb4b7fa6ac12c5d7
Cezary Protasiewicz <cezary.protasiewicz@gmail.com>
2022-04-12 00:49:05 +0200

    szyfrowanie zdjęcia

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let intervalId
-
+let image_data
 function randomDigit() {
     return Math.floor(Math.random() * Math.floor(2));
   }
@@ -127,6 +127,66 @@ function Szyfrowanie(){
     
 }
 
+function Szyfrowanie1(){
+    let text = image_data;
+    let key = document.getElementById("key1").value;
+    let  text_bin =  text2Binary(text)
+    let arr = text_bin.split(' ')
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].length != 7){
+            arr[i] = "0" + arr[i]
+        }
+    }
+    text_bin = arr.join(' ')
+    let result = ''
+    let j = 0
+    let k = 0
+    for(i = 0; i< text_bin.length;i++){
+        if(text_bin[k] == ' '){
+          k++
+          result += ' '
+        }else{      
+        if(j == key.length){
+            j = 0
+        }
+        result += (key[j++] ^ text_bin[k++])
+        }
+    }
+    console.log("text " + text_bin)
+    console.log("key " + key)
+    console.log("result " + result)
+    
+    document.getElementById("textarea").value = result
+    return result
+    
+  }
+
+  function deszyfrowanie1(){
+    let text_bin = document.getElementById("textarea").value;
+    let key = document.getElementById("key1").value;
+    let j = 0;
+    let k = 0
+    let result = ''
+  for(i = 0; i< text_bin.length;i++){
+        if(text_bin[k] == ' '){
+          
+          k++
+          result += ' '
+        }else{     
+         
+        if(j== key.length){
+            j = 0
+        }
+        result += (key[j++] ^ text_bin[k++])
+        }
+    }
+      let converted_result = binaryToText(result)
+      console.log("deszyfr " + result)
+      console.log(converted_result)
+      document.getElementById("textarea1").value = converted_result
+      return converted_result
+    
+}
 
 function text2Binary(string) {
     return string.split('').map(function (char) {
@@ -140,4 +200,24 @@ function binaryToText(binary) {
    return binary.map(elem => String.fromCharCode(parseInt(elem, 2))).join("");
    }
 
+   function previewFile() {
+    const preview = document.querySelector('img');
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+  
+    reader.addEventListener("load", function () {
+      // image to base64
+      preview.src = reader.result;
+      
+      let arr = preview.src.split(",")
+      
+      image_data = arr[1]
+      document.getElementById("textarea1").value = image_data
+    }, false);
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+
+  }
 
